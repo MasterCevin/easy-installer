6fd78e23959b1a4067e245521d8e52727836ac3d
Cevin-Lee Lehrmann <cle@boreus.de>
2020-11-16 23:22:34 +0100

    added README.md

--- main.sh
+++ main.sh
@@ -12,16 +12,3 @@ echo "loading functions, please wait a moment..."
 ##############
 checkpoint=2 #
 ##############
-
-### Begin of Test section ###
-
-header
-
-question="Test123"
-defaultanswer="y"
-question_yn
-
-question="Dies ist ein Test:"
-defaultanswer="Test123"
-question_data
-echo "$answer"
